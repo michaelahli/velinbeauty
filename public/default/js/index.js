a10583de8dc1b18768cde47fcdc081976dd9e672
michaelahli <michael.ahli99@gmail.com>
2020-07-01 20:50:53 +0700

    carousel

--- public/default/js/index.js
+++ public/default/js/index.js
@@ -30,3 +30,22 @@ function toggle() {
         x.style.display = "block";
     }
 }
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
+    });
+});
