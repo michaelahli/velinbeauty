cd97ce34182ecb099a6162fcdc3b12bc6a48781b
michaelahli <michael.ahli99@gmail.com>
2020-07-03 10:28:47 +0700

    button

--- public/default/js/index.js
+++ public/default/js/index.js
@@ -2,7 +2,6 @@ var open = document.getElementById('hamburger');
 var changeIcon = true;
 
 open.addEventListener("click", function () {
-
     var overlay = document.querySelector('.overlay');
     var nav = document.querySelector('.full');
     var icon = document.querySelector('.menu-toggle i');
@@ -50,4 +49,42 @@ $(document).ready(function () {
             }
         }
     });
+    $("a.scrollLinkmobile").click(function (event) {
+        event.preventDefault();
+        var x = document.getElementById("nav");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+        var overlay = document.querySelector('.overlay');
+        var nav = document.querySelector('.full');
+        var icon = document.querySelector('.menu-toggle i');
+
+        overlay.classList.toggle("menu-open");
+        nav.classList.toggle("menu-open");
+
+        if (changeIcon) {
+            icon.classList.remove("fa-bars");
+            icon.classList.add("fa-times");
+
+            changeIcon = false;
+        } else {
+            icon.classList.remove("fa-times");
+            icon.classList.add("fa-bars");
+            changeIcon = true;
+        }
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 500);
+    });
+    $("a.scrollLink").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top
+        }, 500);
+    });
+
+
+
 });
