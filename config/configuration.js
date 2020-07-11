3b68aa19cdacd78d8e02789411776ff7df1455f2
michaelahli <michael.ahli99@gmail.com>
2020-07-12 02:44:27 +0700

    hairproblem

--- config/configuration.js
+++ config/configuration.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoDbUrl: 'mongodb+srv://michaelahli:koko0410@cluster0-osdpx.mongodb.net/velinbeauty?retryWrites=true&w=majority', //'mongodb://localhost:27017/velinbeauty',
+    mongoDbUrl: 'mongodb://localhost:27017/velinbeauty', //'mongodb+srv://michaelahli:koko0410@cluster0-osdpx.mongodb.net/velinbeauty?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
 }
