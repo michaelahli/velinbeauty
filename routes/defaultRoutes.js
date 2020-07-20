01bd3bf773f89eeef1e8708288b57d27979ebcf6
michaelahli <michael.ahli99@gmail.com>
2020-07-20 08:34:12 +0700

    fixed

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const defaultController = require('../controllers/defaultController')
-
+router.all('/*', (req, res, next) => {
+    req.app.locals.layout = 'default';
+    next();
+});
 router.route('/').get(defaultController.index);
 router.route('/customize').get(defaultController.customize).post(defaultController.getProduct);
 router.route('/solve').get(defaultController.solve).post(defaultController.getProblem);
